738f5ff9f68c17c498473e53e0e5989803e7813d
S4M37 <sametoussema@mail.com>
2017-11-22 13:23:02 +0100

    initializing project

---
+++ app/src/main/java/com/appstud/appstud_testcode/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.appstud.appstud_testcode.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appstud.appstud_testcode.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
